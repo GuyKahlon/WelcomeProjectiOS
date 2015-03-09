985cf2cec07f950108aba6f47ca08d8fc78a0296
Guy Kahlon <GuyKahlon@gmail.com>
2015-03-09 22:08:55 +0200

    Update.

--- WelcomeProject/WelcomeProject/Server/WPInnovaServer.m
+++ WelcomeProject/WelcomeProject/Server/WPInnovaServer.m
@@ -16,8 +16,7 @@
 @implementation WPInnovaServer
 - (AFHTTPRequestOperationManager *)manager
 {
-    if (!_manager)
-    {
+    if (!_manager){
         _manager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://10.29.39.35/"]];
         _manager.responseSerializer = [AFHTTPResponseSerializer serializer];
         _manager.requestSerializer  = [AFJSONRequestSerializer serializer];
@@ -25,8 +24,7 @@
     return _manager;
 }
 
-- (void)searchGuestByPhoneNumber:(NSString *)phoneNumber resualtBloack:(WPServerSearchResualt)resualtBloack;
-{
+- (void)searchGuestByPhoneNumber:(NSString *)phoneNumber resualtBloack:(WPServerSearchResualt)resualtBloack{
     [self.manager GET:[NSString stringWithFormat:@"guests/searchByPhone?phoneNumber=%@",phoneNumber]
            parameters:nil
               success:^(AFHTTPRequestOperation *operation, id responseObject) {
@@ -52,35 +50,7 @@
                }];
 }
 
-//- (void)searchGuestByPicture:(NSArray *)arrayImages resualtBloack:(WPServerSearchResualt)resualtBloack;
-//{
-//    [self.manager POST:@"guests/search"
-//            parameters:@{@"pictures":arrayImages}
-//               success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//                   
-//   NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:responseObject
-//                                                              options:NSJSONReadingMutableContainers
-//                                                                error:nil];
-//    id userDetails = jsonObject[@"guest"];
-//    if ([[userDetails description]isEqualToString:@"{}"])
-//    {
-//       if (resualtBloack) {
-//           resualtBloack(NO,jsonObject);
-//       }
-//    }
-//    else{
-//       if (resualtBloack) {
-//           resualtBloack(YES,userDetails);
-//       }
-//     }
-//    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-//       
-//       NSLog(@"Error: %@", error);
-//    }];
-//}
-
 - (void)getHostsListWithResualBlock:(WPServerHostsListResualt)resualtBlock{
-
     //TODO -- support paging (first page number 0 "page": "1", "size")
     [self.manager GET:@"hosts"
            parameters:@{@"size": @(200)}
@@ -101,55 +71,43 @@
          }];
 }
 
-- (void)notifyWithHostId:(NSObject *)hostId guestId:(NSString *)guestId
-{
+- (void)notifyWithHostId:(NSObject *)hostId guestId:(NSString *)guestId{
+
     NSString *url = [NSString stringWithFormat:@"/notifications?hostId=%@&guestId=%@",hostId,guestId];
     
     [self.manager POST:url
             parameters:nil
-               success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                   
+               success:^(AFHTTPRequestOperation *operation, id responseObject) {                   
            NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:responseObject
                                     options:NSJSONReadingMutableContainers
                                     error:nil];
            if ([jsonObject objectForKey:@"errMsg"]) {
-               [[[UIAlertView alloc]initWithTitle:@""
-                                          message:@"Sorry, we couldn't notify your host that you are here. Please ask the guard to call your host."
+               [[[UIAlertView alloc]initWithTitle:@"Sorry 😳"
+                                          message:@"We couldn't notify your host that you are here. \n Please contact our security guard."
                                          delegate:nil
                                 cancelButtonTitle:@"Dismiss"
                                 otherButtonTitles:nil]show];
            }
-
         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
            NSLog(@"Error: %@", error);
         }];
 }
 
-- (void)createGuestWithGuest:(NSDictionary *)guest hostId:(NSString *)hostId
-{
+- (void)createGuestWithGuest:(NSDictionary *)guest hostId:(NSString *)hostId{
+
     [self.manager POST:@"guests"
             parameters:guest
-          success:^(AFHTTPRequestOperation *operation, id responseObject) {
-              
-              NSLog(@"createGuestWithGuest response");
-              
-              
-              NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:responseObject
-                                                                         options:NSJSONReadingMutableContainers
-                                                                           error:nil];
-              id userDetails = jsonObject[@"guest"];
-              if ([[userDetails description]isEqualToString:@"{}"] == NO)
-              {
-                  [self notifyWithHostId:hostId guestId:userDetails[@"id"]];
-              }
-              else
-              {
-//                  if (resualtBloack) {
-//                      resualtBloack(YES,userDetails);
-//                  }
-              }
-              
-                NSLog(@"JSON: %@", responseObject);
+          success:^(AFHTTPRequestOperation *operation, id responseObject) {              
+          NSLog(@"createGuestWithGuest response");
+          NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:responseObject
+                                                                     options:NSJSONReadingMutableContainers
+                                                                       error:nil];
+          id userDetails = jsonObject[@"guest"];
+          if ([[userDetails description]isEqualToString:@"{}"] == NO)
+          {
+              [self notifyWithHostId:hostId guestId:userDetails[@"id"]];
+          }
+          NSLog(@"JSON: %@", responseObject);
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
               NSLog(@"Error: %@", error);
     }];
